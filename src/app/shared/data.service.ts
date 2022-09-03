7165846fc0b2a6c5d6c413e210738705102a31e7
Mary Rose <mary.rose@acquia.com>
2022-09-03 16:08:58 -0700

    add some functioning to template

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -10,7 +10,7 @@ export class DataService {
   //create some mock data we can use
   //set it to an array of objects
   todos: Todo[] = [
-    new Todo('this is a test'),
+    new Todo('this is a test', true),
     new Todo('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
   ]
 
