fefd5a4327051513faaa8e2f1b3160fa49d1b530
Mary Rose <mary.rose@acquia.com>
2022-09-06 13:46:20 -0700

    create tooltip directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EditTodoDialogComponent } from './edit-todo-dialog/edit-todo-dialog.component';
 
 import { MatDialogModule } from '@angular/material/dialog';
+import { TooltipDirective } from './tooltip.directive';
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
     TodoItemComponent,
-    EditTodoDialogComponent
+    EditTodoDialogComponent,
+    TooltipDirective
   ],
   imports: [
     BrowserModule,
